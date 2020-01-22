b5e51d5577dfd46e6ecae8a7fb4a06d4894e5bec
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-01-22 15:47:32 -0800

    added search form

--- app.js
+++ app.js
@@ -12,11 +12,16 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
+  console.log(req.query) // => "{ term: hey" }
+
+  res.render('home')
 })
+// app.get('/', (req, res) => {
+//   // set the url of the gif
+//   const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+//   // render the hello-gif view, passing the gifUrl into the view to be displayed
+//   res.render('hello-gif', { gifUrl })
+// })
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
   const name = req.params.name;
