ce1fdf9bdf5c5bcaafd3e83c0b4ba3456a1262f8
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-01-27 17:06:54 -0800

    adding style

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
 // Require Libraries
 const express = require('express');
 
-// Require tenorjs near the top of the file
-  const Tenor = require("tenorjs").client({
-    // Replace with your own key
-    "Key": "ML08U02YAQGK", // https://tenor.com/developer/keyregistration
-    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
-    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
-});
-
 // App Setup
 const app = express();
 
+const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "UFXFWLXQEZ03", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+app.use(express.static('public'));
+// Middleware
 // Middleware
 const exphbs  = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+
 // Routes
-app.get('/', (req, res) => {
+  // Routes
+  app.get('/', (req, res) => {
     // Handle the home page when we haven't queried yet
     term = ""
     if (req.query.term) {
         term = req.query.term
     }
     // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
-    Tenor.Search.Query(term, "10")
-        .then(response => {
+    Tenor.Search.Query(term, "10").then(response => {
             // store the gifs we get back from the search
             const gifs = response;
             // pass the gifs as an object into the home page
             res.render('home', { gifs })
         }).catch(console.error);
   })
-  
+
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
-  const name = req.params.name;
+
   // render the greetings view, passing along the name
   res.render('greetings', { name });
 })
+
 // Start Server
+
 app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
